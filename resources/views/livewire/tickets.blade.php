75bec701dc7874cd9d5f233fb19754de7cd22896
Galdan Infotech <galdaninfotech@gmail.com>
2024-01-27 16:15:46 +0000

    added modal dialog

--- resources/views/livewire/tickets.blade.php
+++ resources/views/livewire/tickets.blade.php
@@ -61,4 +61,64 @@
             @endforeach
         @endif
     </div>
+
+
+    {{-- Modal Dialog --}}
+    <div id="claim-modal">
+        <x-slot name="header">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                {{ __('Products') }}
+            </h2>
+        </x-slot>
+     
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 overflow-hidden overflow-x-auto bg-white border-b border-gray-200">
+     
+                        <div class="min-w-full align-middle">
+                            <table class="min-w-full border divide-y divide-gray-200">
+                                <thead>
+                                <tr>
+                                    <th class="px-6 py-3 text-left bg-gray-50">
+                                        <span class="text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase">Name</span>
+                                    </th>
+                                    <th class="px-6 py-3 text-left bg-gray-50">
+                                        <span class="text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase">Description</span>
+                                    </th>
+                                    <th class="px-6 py-3 text-left bg-gray-50">
+                                    </th>
+                                </tr>
+                                </thead>
+     
+                                <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                                    @forelse($tickets as $ticket)
+                                        <tr class="bg-white">
+                                            <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                                                {{ $ticket->status }}
+                                            </td>
+                                            <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                                                {{ $ticket->comment }}
+                                            </td>
+                                            <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                                                {{-- Edit Button --}}
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr class="bg-white">
+                                            <td colspan="3" class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                                                No products found.
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+     
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- end Modal Dialog --}}
 </div>
